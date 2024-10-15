96104d9716a6b265f1f49260508e582c041b9dc9
Arif Mulpratama <arifdata7@gmail.com>
2024-10-15 08:54:44 +0700

    minor fix

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import {Game} from 'phaser';
 import gameConfig from './scenes/gameConfig';
 
-export default new Phaser.Game(gameConfig);
+export default new Game(gameConfig);
